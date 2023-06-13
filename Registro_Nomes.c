0217e19b53a45a5caa25a4c0dcd97a144ee16825
luanpalmaa <123501271+luanpalmaa@users.noreply.github.com>
2023-06-13 14:54:31 -0300

    opção de saida
    incluso no sistema a opção de saída

--- Registro_Nomes.c
+++ Registro_Nomes.c
@@ -140,6 +140,7 @@ int main ()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); // fim do menu
 	
 		scanf("%d",&opcao);
@@ -161,6 +162,12 @@ int main ()
 			deletar();
 			break;
 			
+			case 4:
+			printf("Obrigado por utilizar o sistema !");
+			return 0;
+			break;
+			
+			 
 			default:
 			printf("A opção selecionada não está disponivel\n\n");
 			system("pause");
